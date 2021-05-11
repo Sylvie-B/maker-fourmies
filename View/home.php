274d0e8cf4db95f0d67d4e66a98967958cdd23a1
Sylvie-B <unsolo@hotmail.fr>
2021-05-11 21:02:43 +0200

    page : asso, home

--- View/home.php
+++ View/home.php
@@ -1,1 +1,8 @@
+<!--    le mouvement maker   -->
+<section>
+    <div id="definition">
+        <h1>Le mouvement maker : Qu'est-ce c'est ?</h1>
+        <p>Les makers sont ceux qui font, qui fabriquent, bricolent, créent, mais pas seulement.</p>
+        <p>Etre maker c'est aussi un état d'esprit, celui de l'entre-aide et du partage.</p>
+    </div>
 <?php
