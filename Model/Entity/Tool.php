4036130a5913039340cd34961613d09f0952a574
Sylvie-B <unsolo@hotmail.fr>
2021-05-24 23:58:13 +0200

    make Tool

---
+++ Model/Entity/Tool.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class Tool {
+    private ?int $id_tool;
+    private string $tool;
+
+    public function __construct(?int $id_tool, string $tool){
+        $this->id_tool = $id_tool;
+        $this->tool = $tool;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getIdTool(): ?int
+    {
+        return $this->id_tool;
+    }
+
+    /**
+     * @param int|null $id_tool
+     */
+    public function setIdTool(?int $id_tool): void
+    {
+        $this->id_tool = $id_tool;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTool(): string
+    {
+        return $this->tool;
+    }
+
+    /**
+     * @param string $tool
+     */
+    public function setTool(string $tool): void
+    {
+        $this->tool = $tool;
+    }
+}
