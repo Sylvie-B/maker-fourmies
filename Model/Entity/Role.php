7dc3604d1177fa8293710ac831139f100f8c3c0b
Sylvie-B <unsolo@hotmail.fr>
2021-05-24 23:54:28 +0200

    make Role

---
+++ Model/Entity/Role.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class Role {
+    private ?int $id_role;
+    private string $role;
+
+    public function __construct(?int $id_role, string $role){
+        $this->id_role = $id_role;
+        $this->role = $role;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getIdRole(): ?int
+    {
+        return $this->id_role;
+    }
+
+    /**
+     * @param int|null $id_role
+     */
+    public function setIdRole(?int $id_role): void
+    {
+        $this->id_role = $id_role;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $role
+     */
+    public function setRole(string $role): void
+    {
+        $this->role = $role;
+    }
+}
