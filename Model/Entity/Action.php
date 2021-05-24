e637235ee3d32fb781029866f64aa837ef5c87e8
Sylvie-B <unsolo@hotmail.fr>
2021-05-25 00:25:49 +0200

    update type_fk

--- Model/Entity/Action.php
+++ Model/Entity/Action.php
@@ -3,18 +3,18 @@
 
 class Action {
     private ?int $id_act;
-    private string $type;
     private string $title;
     private string $description;
     // starting date
     private string $date;
+    private int $type_fk;
 
-    public function __construct(?int $id_act, $type, $title, $description, $date){
+    public function __construct(?int $id_act, string $title, string $description, string $date, int $type_fk){
         $this->id_act = $id_act;
-        $this->type = $type;
         $this->title = $title;
         $this->description = $description;
         $this->date = $date;
+        $this->type_fk = $type_fk;
     }
 
     /**
@@ -31,22 +31,6 @@ class Action {
     public function setIdAct(?int $id_act): void
     {
         $this->id_act = $id_act;
-    }
-
-    /**
-     * @return string
-     */
-    public function getType(): string
-    {
-        return $this->type;
-    }
-
-    /**
-     * @param string $type
-     */
-    public function setType(string $type): void
-    {
-        $this->type = $type;
     }
 
     /**
@@ -96,4 +80,21 @@ class Action {
     {
         $this->date = $date;
     }
+
+    /**
+     * @return int
+     */
+    public function getTypeFk(): int
+    {
+        return $this->type_fk;
+    }
+
+    /**
+     * @param int $type_fk
+     */
+    public function setTypeFk(int $type_fk): void
+    {
+        $this->type_fk = $type_fk;
+    }
+
 }
