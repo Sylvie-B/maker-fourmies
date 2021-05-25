c23503dd9e0bfbc60cd48b579135bc86f825f749
Sylvie-B <unsolo@hotmail.fr>
2021-05-25 02:15:37 +0200

    create action manager

--- Model/Manager/actionManager.php
+++ Model/Manager/actionManager.php
@@ -8,6 +8,13 @@ class actionManager {
         $this->pdo = $pdo;
     }
 
+    /**
+     * @param string $title
+     * @param string $description
+     * @param $date
+     * @param int $type_fk
+     * @return string
+     */
     public function addAction(string $title, string $description, $date, int $type_fk) {
         $sql = $this->pdo->prepare("
         INSERT INTO action (title, description, date, type_fk)
@@ -19,5 +26,24 @@ class actionManager {
         $sql->bindValue('type_fk', $type_fk, PDO::PARAM_INT);
         $sql->execute();
         return $this->pdo->lastInsertId();
+    }
+
+    /**
+     * @param $id
+     * @return Action
+     */
+    public function getOneAction($id) : Action {
+        $sql = $this->pdo->prepare("SELECT * FROM action WHERE id_act = $id");
+        $sql->execute();
+        $result = $sql->fetch();
+        $action = new Action();
+        if($result){
+            $action->setIdAct($result['id_act']);
+            $action->setTitle($result['title']);
+            $action->setDescription($result['description']);
+            $action->setDate($result['date']);
+            $action->setTypeFk($result['type_fk']);
+        }
+        return $action;
     }
 }
