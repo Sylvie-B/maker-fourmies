2572ae34f47cb820242bb7a5216fc0ec13c004d8
Sylvie-B <unsolo@hotmail.fr>
2021-05-27 10:16:55 +0200

    matter manager

--- Model/Manager/MatterManager.php
+++ Model/Manager/MatterManager.php
@@ -3,9 +3,19 @@
 
 class MatterManager {
     private PDO $pdo;
+    private OriginManager $originManager;
 
     public function __construct ($pdo){
         $this->pdo = $pdo;
+        $this->originManager = new OriginManager($this->pdo);
+    }
+
+    public function addMatter (string $matter, int $origin_fk){
+        $sql = $this->pdo->prepare("INSERT INTO matter (matter, origin_fk) VALUES (':matter', ':origin-fk')");
+        $sql->bindValue(':matter', $matter);
+        $sql->bindValue(':origin_fk', $origin_fk, PDO::PARAM_INT);
+        $sql->execute();
+        return $this->pdo->lastInsertId() !== 0;
     }
 
 
