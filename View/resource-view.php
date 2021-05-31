cd28579987eda9a1a58dce208ad6a187f3e59b06
Sylvie-B <unsolo@hotmail.fr>
2021-05-31 13:33:58 +0200

    setting page resource

--- View/resource-view.php
+++ View/resource-view.php
@@ -1,5 +1,30 @@
 <div id="frame">
     <h1>Ressource</h1>
 </div>
+<div class="frameText">
+    <p>
+        Vous trouverez ici les logiciels gratuits, utilisés et conseillés par les makers.
+    </p>
+</div>
+<div class="frameText">
+    <p>
+        Les formations, école, en présentiel ou en ligne
+    </p>
+</div>
+<div class="frameText">
+    <p>
+        Les tutos
+    </p>
+</div>
+<div class="frameText">
+    <p>
+        Les informations sur le fablab Le L@bo de Fourmies
+    </p>
+</div>
+<div class="frameText">
+    <p>
+        Les infos sur les Makers faire
+    </p>
+</div>
 
 <?php
