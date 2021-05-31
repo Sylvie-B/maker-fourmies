f91f9b37c86841b4244eee8580cf46c1d4c74953
Sylvie-B <unsolo@hotmail.fr>
2021-05-31 14:14:51 +0200

    setting page gallery

--- View/gallery-view.php
+++ View/gallery-view.php
@@ -1,18 +1,55 @@
 <div id="frame">
     <h1>Galerie</h1>
+    <div id="show">
+        <div class="picture">
+            <img class="project" src="" alt="image 1">
+            <span>Projet 1</span>
+        </div>
+        <div class="picture">
+            <img class="project" src="" alt="image 2">
+            <span>Projet 1</span>
+        </div>
+        <div class="picture">
+            <img class="project" src="" alt="image 3">
+            <span>Projet 1</span>
+        </div>
+        <div class="picture">
+            <img class="animation" src="" alt="image 1">
+            <span>Animation 1</span>
+        </div>
+        <div class="picture">
+            <img class="animation" src="" alt="image 2">
+            <span>Animation 1</span>
+        </div>
+        <div class="picture">
+            <img class="animation" src="" alt="image 3">
+            <span>Animation 1</span>
+        </div>
+        <div class="picture">
+            <img class="project" src="" alt="image 1">
+            <span>Projet 2</span>
+        </div>
+        <div class="picture">
+            <img class="project" src="" alt="image 2">
+            <span>Projet 2</span>
+        </div>
+        <div class="picture">
+            <img class="project" src="" alt="image 3">
+            <span>Projet 2</span>
+        </div>
+        <div class="picture">
+            <img class="animation" src="" alt="image 1">
+            <span>Animation 2</span>
+        </div>
+        <div class="picture">
+            <img class="animation" src="" alt="image 2">
+            <span>Animation 2</span>
+        </div>
+        <div class="picture">
+            <img class="animation" src="" alt="image 3">
+            <span>Animation 2</span>
+        </div>
+    </div>
 </div>
-<div id="show">
-    <div class="picture"></div>
-    <div class="picture"></div>
-    <div class="picture"></div>
-    <div class="picture"></div>
-    <div class="picture"></div>
-    <div class="picture"></div>
-    <div class="picture"></div>
-    <div class="picture"></div>
-    <div class="picture"></div>
-    <div class="picture"></div>
-    <div class="picture"></div>
-    <div class="picture"></div>
-</div>
+
 
