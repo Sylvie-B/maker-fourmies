cd7dfd0efbfc8be2b1033da3dc57abb06d81dba3
Sylvie-B <unsolo@hotmail.fr>
2021-06-05 00:54:04 +0200

    todo

--- View/gallery-view.php
+++ View/gallery-view.php
@@ -1,17 +1,11 @@
 <h1>Galerie</h1>
 <div id="mosaic">
-    <div>
-        <img class="picture" src="../img/action/repose-arc.png" alt="image 1">
-        <span>titre</span>
-    </div>
-    <div>
-        <img class="picture" src="../img/action/support.jpg" alt="image 1">
-        <span>titre</span>
-    </div>
-    <div>
-        <img class="picture" src="../img/action/japan-ari-01.jpg" alt="image 1">
-        <span>titre</span>
-    </div>
+<!--    todo create element    -->
+<!--    <div>   -->
+<!--        <img class="picture" src="../img/action/repose-arc.png" alt="image 1">  -->
+<!--        <span>titre</span>  -->
+<!--    </div>  -->
+
 </div>
 
 
