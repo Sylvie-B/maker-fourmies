e81b6717ac8836b141a70288f75cb1de4c147f7a
Sylvie-B <unsolo@hotmail.fr>
2021-06-06 18:39:31 +0200

    admin menu

--- View/admin-view.php
+++ View/admin-view.php
@@ -1,25 +1,30 @@
 <div>
     <h1>ADMINISTRATEUR</h1>
 </div>
-<?php
-/**
- *  new action form
- */
-require_once $_SERVER['DOCUMENT_ROOT'] . "/View/operation-view.php";
 
-/**
- * action's list
- */
+<!-- principals entities -->
+<ul id="first">
+  <li class="entity">User</li>
+  <li class="entity">Action</li>
+  <li id="others">Base de données</li>
+</ul>
 
+<!-- others entities -->
+<ul id="second">
+    <li class="entity">Image</li>
+    <li class="entity">Matiére</li>
+    <li class="entity">Origine</li>
+    <li class="entity">Role</li>
+    <li class="entity">Technique</li>
+    <li class="entity">Outils</li>
+    <li class="entity">Type</li>
+</ul>
 
-
-/**
- * update action form
- */
-
-
-
-/**
- * delete action
- */
+<!-- operation -->
+<ul id="operation">
+    <li class="action">Ajouter</li>
+    <li class="action">Voir la liste</li>
+    <li class="action">Mettre à jour</li>
+    <li class="action">Supprimer</li>
+</ul>
 
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
-// on click verify that form is not empty
-// get field, label and button
+/* inscription & connexion */
+
+// get fields, labels and button
 let field = document.getElementsByTagName('input');
 let btn = document.getElementById('validate');
 let label = document.getElementsByTagName('label');
 
-// get the place where display text
+// get the place where display error text
 let target = document.getElementById('target');
 
 // create element witch contain the text
 let info = document.createElement('p');
 info.style.textAlign = "center";
 
-// on click count the length of each field
+// on click verify each field is not empty
 if(btn){
     btn.addEventListener('click', function (event){
         info.innerHTML = "Veuillez remplir le(s) champs :";
@@ -22,5 +23,32 @@ if(btn){
             }
         }
         target.appendChild(info);
+    })
+}
+
+/* admin mod */
+// on click show action choice
+let entity = document.getElementsByClassName('entity');
+let operation = document.getElementById('operation');
+let others = document.getElementById('others');
+let second = document.getElementById('second');
+let actions = document.getElementsByClassName('action');
+
+// first menu on click show operations or others entities
+for (let i = 0 ; i < entity.length ; i++){
+    entity[i].addEventListener('click', function (){
+        entity[i].style.backgroundColor = 'white';
+        operation.style.visibility = 'visible';
+    })
+}
+
+others.addEventListener('click', function (){
+    others.style.backgroundColor = 'white';
+    second.style.visibility = 'visible';
+})
+
+for (let i = 0 ; i < actions.length ; i++){
+    actions[i].addEventListener('click', function (){
+        actions[i].style.backgroundColor = 'white';
     })
 }
