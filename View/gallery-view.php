f537c9f7f037761d47f0bf052b4c382cd145fa31
Sylvie-B <unsolo@hotmail.fr>
2021-06-11 14:14:44 +0200

    imgage's title

--- View/gallery-view.php
+++ View/gallery-view.php
@@ -2,7 +2,9 @@
 <!--display images-->
     <?php
         foreach ($var as $item){
-            echo "<img class='imgFrame' src='/img/action/" . $item->getImage() . "' alt = '" . $item->getImageTitle() . "'>";
+            echo "
+            <img class='imgFrame' src='/img/action/" . $item->getImage() . "' alt = '" . $item->getImageTitle() . " ' title='" . $item->getImageTitle() . "'>
+            ";
         }
     ?>
 </div>
